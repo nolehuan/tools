87182a08ff6d76deb6425392d3410275b4267cc4
nolehuan <943126535@qq.com>
2022-05-23 17:33:43 +0800

    Update

--- images2video.py
+++ images2video.py
@@ -3,19 +3,18 @@ import glob
 import time
 import numpy as np
 
-images = glob.glob('../LoFTR/assets/odometry/09/result/*.png')
-# images = sorted(glob.glob('../LoFTR/assets/odometry/09/masked/*.png'))
+# images = glob.glob('../LoFTR/assets/odometry/09/result/*.png')
+images = sorted(glob.glob('../LoFTR/assets/odometry/09/masked/*.png'))
 
 
 fps = 20
-size = (729, 108)
-# fps = 30
-# size = (1226, 370)
-vWriter = cv2.VideoWriter('../loftr.mp4', cv2.VideoWriter_fourcc(*"mp4v"), fps, size)
-# vWriter = cv2.VideoWriter('../ld.mp4', cv2.VideoWriter_fourcc(*"mp4v"), fps, size)
+# size = (729, 108)
+size = (1226, 370)
+# vWriter = cv2.VideoWriter('../loftr.mp4', cv2.VideoWriter_fourcc(*"mp4v"), fps, size)
+vWriter = cv2.VideoWriter('../ld.mp4', cv2.VideoWriter_fourcc(*"mp4v"), fps, size)
 
-for i in range (len(images)): # 152-636
-# for i in range (60, 297): # 152-636
+# for i in range (len(images)): # 152-636
+for i in range (60, 297): # 152-636
     path = images[i]
     frame = cv2.imread(path, -1)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
