d251311f4e7bd169fd50e6d989720a708bc26f27
nolehuan <943126535@qq.com>
2022-06-13 09:53:06 +0800

    update

--- learning/spp.py
+++ learning/spp.py
@@ -1,4 +1,4 @@
-from numpy import pad
+import time
 import torch
 import torch.nn as nn
 
@@ -41,10 +41,55 @@ class Focus(nn.Module):
         x = self.act(x)
         return x
 
-
-
 x = torch.randn([1, 64, 32, 32])
 spp = SPP(64, 128)
 print(spp(x).shape)
 # foc = Focus(64, 128)
 # print(foc(x).shape)
+
+
+class SPP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.maxpool1 = nn.MaxPool2d(5, 1, padding=2)
+        self.maxpool2 = nn.MaxPool2d(9, 1, padding=4)
+        self.maxpool3 = nn.MaxPool2d(13, 1, padding=6)
+
+    def forward(self, x):
+        o1 = self.maxpool1(x)
+        o2 = self.maxpool2(x)
+        o3 = self.maxpool3(x)
+        return torch.cat([x, o1, o2, o3], dim=1)
+
+class SPPF(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.maxpool = nn.MaxPool2d(5, 1, padding=2)
+
+    def forward(self, x):
+        o1 = self.maxpool(x)
+        o2 = self.maxpool(o1)
+        o3 = self.maxpool(o2)
+        return torch.cat([x, o1, o2, o3], dim=1)
+
+def main():
+    input_tensor = torch.rand(8, 32, 16, 16)
+    spp = SPP()
+    sppf = SPPF()
+    output1 = spp(input_tensor)
+    output2 = sppf(input_tensor)
+
+    print(torch.equal(output1, output2))
+
+    t_start = time.time()
+    for _ in range(100):
+        spp(input_tensor)
+    print(f"spp time: {time.time() - t_start}")
+
+    t_start = time.time()
+    for _ in range(100):
+        sppf(input_tensor)
+    print(f"sppf time: {time.time() - t_start}")
+
+if __name__ == '__main__':
+    main()
