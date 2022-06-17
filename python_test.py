c401be4fd514a83d2656b0f487c0a99d3b05f64c
nolehuan <943126535@qq.com>
2022-06-17 10:03:21 +0800

    update

--- python_test.py
+++ python_test.py
@@ -3,8 +3,34 @@ import random
 import uuid
 import numpy as np
 
-np.random.uniform(-1, 1, 3)
 
+print("{:06}".format(1))
+
+print(np.max((0, 1)))
+
+x = tuple(["s", "d", "f"])
+print(x)
+
+print(os.path.dirname(os.path.dirname(__file__)))
+
+print(os.getenv("HOME", None))
+
+# x = range(10)[:12]
+x = range(10)[:5]
+
+x = np.zeros((2, 3))
+print(x)
+
+x = np.random.randint(1,3,3)
+print(x.shape)
+x = np.expand_dims(x, 1)
+x = np.expand_dims(x, 1)
+print(x.shape)
+x = np.hstack((x, x))
+print(x.shape)
+
+print(np.random.uniform(-1, 1, 3))
+print(np.random.randint(0, 2, 3))
 
 seed = uuid.uuid4().int % 2**32
 print(seed)
