2c0ee773acdacbda06d052e00e42f4353a9862f0
nolehuan <943126535@qq.com>
2022-06-28 15:52:06 +0800

    update

--- learning/convbn.py
+++ learning/convbn.py
@@ -1,5 +1,6 @@
+import torch.nn as nn
 
-''' conv + bn acceleration
+# conv + bn acceleration
 module = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1)
 bn_module = nn.BatchNorm2d(16, affine=True)
 
@@ -15,4 +16,4 @@ b.add_(-bn_module.running_mean).mul_(invstd)
 if bn_module.affine:
     w.mul_(bn_module.weight.data.view(*ws).expand_as(w))
     b.mul_(bn_module.weight.data).add_(bn_module.bias.data)
-'''
+
