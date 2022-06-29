1f4f25f06f25d55108b28797681d77a1cd781e23
nolehuan <943126535@qq.com>
2022-06-29 20:40:08 +0800

    update

--- learning/conv.py
+++ learning/conv.py
@@ -40,13 +40,34 @@ class Conv(object):
                 out[i, j] = np.sum(fea_win * kernel)
         return out
     
-    def conv_mat(self):
+    def conv2d_mat(self):
         b, c_in, h, w = self.inputs.shape
         c_out, _, k, _  = self.weights.shape
-        outputs = []
+
         fea_pad = np.zeros([b, c_in, h + 2 * self.padding, w + 2 * self.padding], np.float32)
         fea_pad[..., self.padding:h+self.padding, self.padding:w+self.padding] = self.inputs
-        pass
+
+        outputs = []
+        for m in range(b):
+            output = []
+            for n in range(c_out):
+                kernel = self.weights[n]
+                kernel = kernel.reshape(c_in * k * k, 1)
+                h_out = (h + 2 * self.padding - k) // self.stride + 1
+                w_out = (w + 2 * self.padding - k) // self.stride + 1
+                mat = np.zeros([h_out * w_out, c_in * k * k], np.float32)
+                row = 0
+                for i in range(h_out):
+                    for j in range(w_out):
+                        fea_win = fea_pad[m, :, i * self.stride : i * self.stride + k, j * self.stride : j * self.stride + k]
+                        mat[row] = fea_win.flatten()
+                        row += 1
+                out = np.dot(mat, kernel).reshape(h_out, w_out)
+                output.append(out)
+            outputs.append(output)
+        
+        return np.asarray(outputs, np.float32)
+
 
 if __name__=='__main__':
     inputs = np.array([[
@@ -81,6 +102,9 @@ if __name__=='__main__':
     ]])
     print(weights.shape)
     conv = Conv(inputs, weights)
+    out_mat = conv.conv2d_mat()
+    print(out_mat)
+    print(out_mat.shape)
     out = conv.conv2d()
     print(out)
     print(out.shape)
