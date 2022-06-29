eea9ae2af87b41f94cfd4833613401fbc8636612
nolehuan <943126535@qq.com>
2022-06-29 20:02:09 +0800

    update

--- learning/conv.py
+++ learning/conv.py
@@ -4,16 +4,16 @@ import numpy as np
 
 class Conv(object):
     def __init__(self, inputs, weights, stride=1, padding=1) -> None:
+        '''
+        inputs: b * c_in * h * w
+        weights: c_in * k * k * c_out
+        '''
         self.inputs = np.asarray(inputs, np.float32)
         self.weights = np.asarray(weights, np.float32)
         self.stride = stride
         self.padding = padding
 
     def conv2d(self):
-        '''
-        inputs: b * c_in * h * w
-        weights: c_in * k * k * c_out
-        '''
         b, c_in, h, w = self.inputs.shape
         c_out, _, k, _  = self.weights.shape
         outputs = []
@@ -39,6 +39,14 @@ class Conv(object):
                 fea_win = fea_pad[:, i * self.stride : i * self.stride + k, j * self.stride : j * self.stride + k]
                 out[i, j] = np.sum(fea_win * kernel)
         return out
+    
+    def conv_mat(self):
+        b, c_in, h, w = self.inputs.shape
+        c_out, _, k, _  = self.weights.shape
+        outputs = []
+        fea_pad = np.zeros([b, c_in, h + 2 * self.padding, w + 2 * self.padding], np.float32)
+        fea_pad[..., self.padding:h+self.padding, self.padding:w+self.padding] = self.inputs
+        pass
 
 if __name__=='__main__':
     inputs = np.array([[
