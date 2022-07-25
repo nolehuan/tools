10bff3768395eff448a5ce943ae4fc4e4a156504
nolehuan <943126535@qq.com>
2022-07-25 20:30:58 +0800

    update

--- python_test.py
+++ python_test.py
@@ -7,12 +7,21 @@ import math
 import io
 import itertools
 import datetime
+import scipy.special
 
+idx = np.arange(3) + 1
+print(idx)
 
-
-
-
-
+out = np.array([[[0,1,2,3],[4,5,6,7],[8,9,10,11]], [[12,13,14,15],[16,17,18,19],[20,21,22,23]]])
+print(out)
+print(out.shape)
+out = out[:, ::-1, :]
+print(out)
+out_argmax = out.argmax(0)
+print(out_argmax)
+print(out_argmax.shape)
+out = scipy.special.softmax(out[:-1, :, :], axis=0)
+print(out)
 
 x = np.array([[2, 4, 5, 1], [3, 4, 9, 0]])
 print(x.shape)
